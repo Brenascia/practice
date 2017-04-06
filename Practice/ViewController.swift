4177df3ad2ae778cddf29e4489794c94be6c0d9d
Bridgette Jefferson <BridgetteJefferson@Bridgettes-MacBook-Pro.local>
2017-04-06 15:03:18 -0400

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Bridgette Jefferson on 4/6/17.
+//  Copyright © 2017 Bridgette Jefferson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
